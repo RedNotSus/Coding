7eba14ea1ea73b066a081ee07f149459bab4cb61
RedNotSus <88468801+RedNotSus@users.noreply.github.com>
2022-07-15 20:23:49 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to the Jiayang App, Please sign in below!")
+user = input("Please enter your JID number ")
+password = input("Please enter your password ")
+if user == ("1"):
+    print("Checking...")
+
+    if password == ("i am cool"):
+        print("Account found, logging in")
+
+else: 
+    print("Wrong Username or password!") 
